97a9d68c774d0c74d011b3c67d0aa44ff0304d69
agumig <agrosso@frba.utn.edu.ar>
2017-03-23 15:27:46 -0300

    Testbench de Codificador Manchester

--- tb_manch_mod.v
+++ tb_manch_mod.v
@@ -1,9 +1,12 @@
-//-------------------------------------------------
-//          Test Bench
-//-----------------------------------------------------------
- `timescale 1us/1us	//Dividir el eje de tiempo en unidades de 10us
+/*----------------------------------------------------------------
+--          Test Bench						--
+-- Archivo a testear: 	manch_mod.v				--
+-- Descripcion:		DUT codificador manchester		--
+----------------------------------------------------------------*/
 
-module man_mod_tb;
+ `timescale 1us/1us	//Dividir el eje de tiempo en unidades de 1us
+
+module man_mod_tb;	//Modulo de test bench
 
 // Entradas del DUT son reg
   reg clk;
@@ -12,7 +15,7 @@ module man_mod_tb;
 //   Las salidas del DUT son wire
   wire out_data;
   
-  man_mod u1(clk, in_enable, in_data, out_data);
+  man_mod u1(clk, in_enable, in_data, out_data);	//DUT a testear
 //----------------------------------------------------------
 // Inicializo
 // Inicializar todo para que no quede en estados "fantasmas"
@@ -24,36 +27,34 @@ module man_mod_tb;
 
       end
 //----------------------------------------------------------
-// Creo un clock de 50Mhz
+// Creo un clock de 500khz
     always
-      #1 clk = ~clk;   // Cada 10 ns invierto
+      #1 clk = ~clk;   // Cada 1 us invierto
 	 
 //-----------------------------------------------------------
 
     initial
       begin
 //      forever #10 clk = ~clk; // generate a clock	
-	#10 repeat(4) #8 in_data = ~in_data;
-// 	#200 in_data=1'b1;	//200ns despues del inicio	
-	#200 in_enable=1'b1; 	//200ns despues de la instruccion anterior
-	#10 repeat(4) #8 in_data = ~in_data;
-	in_data=1'b0;
-	#16	in_data = 1'b1;
-	#16 in_enable=1'b0; 	
-	#32 in_enable=1'b1; 
+	#10 repeat(4) #8 in_data = ~in_data;		//10us del arranque, switcheo in_data cada 8us
+	#36 in_enable=1'b1; 				//200us despues de la instruccion anterior habilito el dispositivo
+	#16 repeat(4) #16 in_data = ~in_data;		//8us despues de la instruccion anterior switcheo in_data cada 24us
+	in_data=1'b0;					//Pongo a 0 in_data
+	#16	in_data = 1'b1;				//16us despues, lo pngo a 1
+
+// 	#16 in_enable=1'b0; 	
+// 	#32 in_enable=1'b1; 
 //  	#560 repeat(10) #10 in_data = ~in_data;
-	#32 $finish;
+	#32 in_enable=1'b0; 				//Deshabilito el dispositivo
+	#16 $finish;					//Finalizo la simulación
       end
  
-    initial
-      $monitor("clk=%b,in_data=%b,in_enable=%b",clk,in_data,in_enable);
+//     initial	
+//       $monitor("clk=%b,in_data=%b,in_enable=%b",clk,in_data,in_enable);	//Imprimo en consola los datos
  
     initial
     begin
-      $dumpfile("man_mod_tb.vcd");
-      $dumpvars(0,man_mod_tb);
+      $dumpfile("man_mod_tb.vcd");	//Creo el archivo para simular con GTKWave
+      $dumpvars(0, man_mod_tb);
     end
 endmodule
-    
-    
-    
