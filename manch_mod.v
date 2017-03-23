b7303187719d6ce86b50dce22c5955157597428d
agumig <agrosso@frba.utn.edu.ar>
2017-03-23 15:27:16 -0300

    Codificador Manchester. Código Verilog Funcional

--- manch_mod.v
+++ manch_mod.v
@@ -1,45 +1,65 @@
-/*------------------------------------------------------------------
---                     Codificador Manchester                     --
--- * 1.0                                                          --
+/*------------------------------------------------------------------------
+--                     Codificador Manchester                     	--
+-- * 1.0                                                          	--
+-- Descripcion:                                                   	--
+--    	Codificador manchester 		                          	--
+--Ingresa el frame a enviar y sale codificado (codificacion manchester) --
+--	Test bench de este archivo en tb_manch_mod.v			--
+------------------------------------------------------------------------*/
 
--- Descripcion:                                                   --
---    Codificador manchester 		                          --
-------------------------------------------------------------------*/
+module man_mod #(parameter N = 3)(clk, in_enable, in_data, out_data);
 
-
-module man_mod #(parameter N = 4)(clk, in_enable, in_data, out_data);
-
-input clk,in_data,in_enable;
-output out_data;
+input clk;		//Clock de fc/16 en RFID (847,5 KHz)
+input in_data;		//Informacion - ETU de 106Kb/s (8 clocks dentro de cada ETU)
+input in_enable;	//Bit de habilitacion del modulo
+output reg out_data;	//Salida del codificador manchester
 
 reg  [N-1:0] count;	//Cuento los cuatro clock para estar en la mitad del ETU
-reg out_data;	//Salida del codificador
+reg etu;		//0 si se está en la primer mitad del etu - 1 si se está en la segunda mitad
 
-  always @ (negedge clk, negedge in_enable)
-    begin
-      if(~in_enable) begin
+  always @ (negedge clk, negedge in_enable) begin
+      if(~in_enable) begin	//Cuando esta desabilitado el modulo, pongo la salida y el contador en 0 (cero)
 	out_data <= 1'b0;
-	count <= 4'b0;
+	etu <= 1'b0;
+	count <= {N{1'b0}};
+      end else begin
+      count <= count +1;
+      if(count == 3'b100) begin	//Cuando cuento la mitad de los clocks que entran en un etu
+	etu = ~ etu;		//invierto el etu
+	count <= 3'b001;		//reseteo el contador
       end
-	else if(in_data) begin
-	    out_data <= clk;
-	    count <= count + 4'b0001;
-	    end else begin
-	      out_data <= clk;
+      if(~etu) begin		//Si estoy en la primera mitad del ETU
+	if(in_data) begin	//Si estoy en presencia de un 1 como dato
+  	    out_data <= clk;	//Copio el clk
+  	end else begin 		//Si estoy en presencia de un 0 como dato
+	    out_data <= 1'b1;	//Pongo en 1 la salida
 	    end
+      end else begin		//Si estoy en la segunda mitad de ETU
+	if(~in_data) begin	//Si estoy en presencia de un 0 como dato
+  	    out_data <= clk;	//Copio el clk
+  	end
+	end	
+	end
   end
-	    
-	    
-  always @ (posedge clk, negedge in_enable)
-    begin
-       if(~in_enable) begin
-       	count <= 4'b0;
+	 	    
+  always @ (posedge clk, negedge in_enable) begin
+       if(~in_enable) begin	//Cuando esta desabilitado el modulo, pongo la salida y el contador en 0 (cero)
 	out_data <= 1'b0;
-      end
-        else if(in_data && count == {N{1'b1}}) begin
-	  out_data <= 1'b1;
-	  count <= {N{1'b0}};
-	  end
-  end
-  endmodule
-	  
+	etu <= 1'b0;
+	count <= {N{1'b0}};
+      end else begin
+      
+      if(~etu) begin		//Si estoy en la primera mitad del ETU
+	if(in_data) begin	//Si estoy en presencia de un 1 como dato
+  	    out_data <= clk;	//Copio el clk
+  	end else begin 		//Si estoy en presencia de un 0 como dato
+	    out_data <= 1'b1;	//Pongo en 1 la salida
+	    end
+      end else begin		//Si estoy en la segunda mitad de ETU
+	if(~in_data) begin	//Si estoy en presencia de un 0 como dato
+  	    out_data <= clk;	//Copio el clk
+  	end
+	end
+	end
+  end  
+endmodule
